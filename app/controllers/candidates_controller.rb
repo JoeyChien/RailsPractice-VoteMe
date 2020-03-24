d00f11e1bad043fda2a8040ce121344562a172ed
Joey <aileen60222@gmail.com>
2020-03-24 14:31:18 +0800

    add detail page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,30 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
+  end
+
+  def show
+    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+    
+    if @candidate.save
+      flash[:notice] = "Candidate created"
+      redirect_to '/candidates'
+    else
+    # 借new這個頁面再渲染一次  
+      render :new 
+    end
   end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)   
+  end  
 end
