fa4004a2ab2046586bfefcc33b365baa8bd0ef33
Joey <aileen60222@gmail.com>
2020-03-24 15:44:53 +0800

    edit,delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :candidates
+  resources :candidates do
+    member do
+      post:vote
+      # 路徑會帶id
+    end
 
-  get '/hello.php', to: 'candidates#index'
+    # collection do
+    #   post :vote
+    #   # 路徑不會帶id
+    # end
+  end
+  # post '/condidate/:id/vote', to: 'candidate#vote'
+
+  # get '/hello.php', to: 'candidates#index'
 end
